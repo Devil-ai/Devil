b0329030475be43430bc6f995b3557e442a885b2
Amn008 <amandalal025@Gmail.com>
2018-09-06 13:23:42 +0530

    Update google_speech_to_text.py

--- speech to text/google_speech_to_text.py
+++ speech to text/google_speech_to_text.py
@@ -1,5 +1,5 @@
 import speech_recognition as sr
-cred="AIzaSyCRptLxqzkiWw4JBwIefCcMuKDJaWGwadE"
+cred=""
 with sr.AudioFile("output.wav") as source:
 
 	print(sr.Recognizer().recognize_google(sr.Recognizer().record(source), cred))
